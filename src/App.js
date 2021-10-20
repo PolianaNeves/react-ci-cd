619bb9929116a849385c50378dc3f5d5a63acccf
PolianaNevesTW <poliana.neves@thoughtworks.com>
2021-10-20 11:28:51 -0300

    fix: adding text to link

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn
+          Learn React
         </a>
+        auto deploy
       </header>
     </div>
   );
